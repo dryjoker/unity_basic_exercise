4327e3f6e52d864f1e43afd137b3260d4fcc26b4
Jacob <koaladaydreamer@gmail.com>
2017-02-14 20:03:53 +0800

    Loop_Learn_example
    Loop_Learn

---
+++ 02_while_for_unity_example/Assets/loopLearn.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loopLearn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	//班上有五位同學你喜歡第四個同學
+        //                 A        C
+        for(int i=1 ;   i<=4    ;   i++)
+        {   //B
+            if (i == 4)
+                Debug.Log("for_情書給第"+i+"個同學");
+            
+        }
+
+        int w = 1;
+        while( w <=4 )
+        {
+            Debug.Log("while_情書傳給第"+ w +"位同學");
+            w++;
+        }
+
+        int d = 1;
+
+        do
+        {
+            Debug.Log("do_while_情書傳給第" + d + "位同學");
+            d++;
+        }
+        while (d<=4);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
