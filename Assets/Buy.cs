9552be3ecb0193c1f21c0a8d7c2eef6a0f6178e7
Sam <koaladaydreamer@gmail.com>
2017-02-14 19:55:11 +0800

    unity_basic_if_else_relation

---
+++ Assets/Buy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {//開始方法
+        Debug.Log("你好 my C# unity project");
+        //Unity 提供的方法 可以輸出一組文字 
+
+        string 紙條 = "去幫我買pizza";
+        int 奶茶數量 = 2;
+        float 奶茶甜度 = 0.3f;
+        bool 要切 = false; //pizza 是否要切 是否?
+
+        Debug.Log(紙條);
+
+        int pizza張數 = 3;
+
+        Debug.Log("我要"+pizza張數 + "張pizza");
+
+        string 口味 = "海鮮總匯";
+        Debug.Log("老闆我要" + 口味 + "口味的pizza" + pizza張數 +"張");
+
+        Debug.Log("還有" + 奶茶數量 + "杯奶茶");
+        Debug.Log("奶茶甜度" + 奶茶甜度);
+
+        if(要切)
+        {
+            Debug.Log("阿對了 老闆 我的pizza要切");
+        }
+        else
+        {
+            Debug.Log("阿對了 老闆 我的pizza不要切");
+        }
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/shopping.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shopping : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string 產品名稱 = "好折凳";
+        string 產品別稱 = "七大武器之首";
+
+        Debug.Log("主持人:各位觀眾 走過別錯過");
+        Debug.Log("主持人:今天介紹" + 產品名稱 + "也就是傳說中的" + 產品別稱 + "他的特色有....");
+
+        Color 顧客喜好顏色 = Color.blue;
+        Color 折凳顏色 = Color.blue;//藍色
+        //Unity內建提供的顏色
+
+        int 顧客需要的大小 = 90;
+        int 折凳寬 = 90;
+
+
+        if (顧客喜好顏色 == 折凳顏色 || 顧客需要的大小 < 折凳寬 )
+        {
+            Debug.Log("顧客:我要買這個折凳 這個顏色我喜歡");
+        }
+        else
+        {
+            Debug.Log("顧客: 我不感興趣");
+        }
+
+        int 產品價格 = 540;
+        int 其他同類產品最低價 = 550;
+        bool 產品多用途 = true;
+        string 產品品牌 = "牛頭牌";
+
+        //  600         550                 true
+        if(產品價格 < 其他同類產品最低價 && 產品多用途)
+        {
+            Debug.Log("龜毛顧客: 多方考慮後買下"+ 產品名稱 + "會很划算");
+        }
+        else
+        {
+            Debug.Log("龜毛顧客: 我不要買這了");
+        }
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
